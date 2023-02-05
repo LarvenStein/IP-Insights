6e7703b432b853fa8bd3297752e4c509ebbc4097
LarvenStein <steinlarve@steinlarve.de>
2023-02-05 21:22:33 +0100

    Added Firefox Extension

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php
+echo '
+<br>
+<footer>
+        < <a href="https://ip.steinlarve.de?query='.$query.'">Start</a> / <a href="https://ip.steinlarve.de/whois?query='.$query.'">WHOIS</a> >
+        <br>
+        <br>
+        < <a href="https://discord.com/api/oauth2/authorize?client_id=992069594900611213&permissions=67584&scope=bot" target="_blank">Discord Bot</a> / <a href="https://addons.mozilla.org/en-US/firefox/addon/ip-insights/" target="_blank">Firefox Extension</a> >
+        <br>
+        <br>
+        < <a href="https://github.com/LarvenStein/IP-Lookup" target="_blank">GitHub</a> / <a href="privacy/">Datenschutz</a> >
+</footer>
+';
+?>
