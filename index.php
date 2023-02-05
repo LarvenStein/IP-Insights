6cb7aa5f9f0924673312adddd3fcd31be96a8cda
LarvenStein <steinlarve@steinlarve.de>
2023-02-05 20:59:46 +0100

    Ping & more "DRY"

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="de">
+<html lang="en">
 <head>
 <link rel="stylesheet" href="style.css">
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
@@ -14,19 +14,35 @@
     <title>IP Lookup</title>
 </head>
 <body>
-<div class="center">
+    <header>
     <h1>IP Lookup</h1>
     <form method="get">
-        <input type="text" id="query" name="query" placeholder="IP Adresse oder Domain"><button type="submit" class="search">Suchen</button>
+        <?php
+        error_reporting(0);
+        echo '
+        <input type="text" id="query" name="query" value="'.$_GET['query'].'" placeholder="IP Adress or Domain"><button type="submit" class="search">Search</button>
+        ';
+        ?>
     </form>
     <br>
-</div>
+    </header>
+    <script src="ping.js"></script>
+    <br>
 <?php
-    if(isset($_GET['query'])) {
+    if(!isset($_GET['query'])) {
+        $query = $_SERVER['REMOTE_ADDR'];
+    } else {
+        $query = $_GET['query'];
+        
+    }
 
         $ipdata = [];
         
-        $ipdatajson = file_get_contents('http://ip-api.com/json/'.$_GET['query'].'?fields=28569599');
+        if (str_contains($query, ':')) {
+            $query = explode(':', $_GET['query'])[0];
+        } 
+
+        $ipdatajson = file_get_contents('http://ip-api.com/json/'.$query.'?fields=28569599');
         $ipdata = json_decode($ipdatajson, true);
 
         if($ipdata['hosting'] == 'true') {
@@ -48,8 +64,8 @@
         if($ipdata['status'] == 'success') {
 
         echo "
-        <div class='center'>
-        <h3>Die IP Adresse &#34;".$ipdata['query']."&#34; gehört zu dieser Region:</h3>
+<div class='center'>
+        <h3>The IP address &#34;".$ipdata['query']."&#34; belongs to this region:</h3>
         <div id='map'></div>
         </div>
         <script>
@@ -59,247 +75,115 @@
         attribution: '© OpenStreetMap'
         }).addTo(map);
         L.marker([".$ipdata['lat'].", ".$ipdata['lon']."]).addTo(map)
-        .bindPopup('<table><tr><td></td><td><b>".$ipdata['query']."</b></td></tr><tr><td><i>Stadt:</i></td><td>".$ipdata['city']." (".$ipdata['zip'].")</td></tr><tr><td><i>Region:</i></td><td>".$ipdata['regionName']." (".$ipdata['region'].")</td></tr><tr><td><i>Land: </i></td><td>".$ipdata['country']." (".$ipdata['countryCode'].")</td></tr><tr><td><i>Zeitzone: </i></td><td>".$ipdata['timezone']."</td></tr><tr><td><i>ISP:</i></td><td>".$ipdata['isp']."</td></tr><tr><td><i>Datacenter: </i></td><td>".$datacenter."</td></tr><tr><td><i>Mobil: </i></td><td>".$mobile."</td></tr></table>')
+        .bindPopup('<table><tr><td></td><td><b>".$ipdata['query']."</b></td></tr><tr><td><i>City:</i></td><td>".$ipdata['city']."</td></tr><tr><td><i>Region:</i></td><td>".$ipdata['regionName']." (".$ipdata['region'].")</td></tr><tr><td><i>Country: </i></td><td>".$ipdata['country']." (".$ipdata['countryCode'].")</td></tr><tr><td><i>Time zone: </i></td><td>".$ipdata['timezone']."</td></tr><tr><td><i>ISP:</i></td><td>".$ipdata['isp']."</td></tr><tr><td><i>Datacenter: </i></td><td>".$datacenter."</td></tr><tr><td><i>Mobile: </i></td><td>".$mobile."</td></tr></table>')
         .openPopup();
         </script>
 
         <div class='center'>
-        <h2>Genaue Informationen</h2>
-        <div class='exactinfo'>
-        <table>
+         <h2>Detailed information</h2>
+         <div class='exactinfo'>
+         <table>
+             <tr>
+                 <td><i>Search: </i></td>
+                 <td></td>
+                 <td> ".$query."</td>
+             </tr>
+             <tr>
+             <td><i>IP: </i></td>
+             <td></td>
+             <td> ".$ipdata['query']."</td>
+             </tr>
             <tr>
-                <td><i>Suche: </i></td>
-                <td></td>
-                <td> ".$_GET['query']."</td>
-            </tr>
-            <tr>
-            <td><i>IP: </i></td>
-            <td></td>
-            <td> ".$ipdata['query']."</td>
-            </tr>
-            <tr>
-            <td><i>LAT/LON: </i></td>
-            <td></td>
-            <td> ".$ipdata['lat']."/".$ipdata['lon']."</td>
-            </tr>
-            <tr>
-            <td><i>Stadt: </i></td>
-            <td></td>
-            <td> ".$ipdata['city']."</td>
-            </tr>
-            <tr>
-            <td><i>Postleitzahl: </i></td>
-            <td></td>
-            <td> ".$ipdata['zip']."</td>
-        </tr>
-        <tr>
-        <td><i>Region: </i></td>
-        <td></td>
-        <td> ".$ipdata['regionName']." (".$ipdata['region'].")</td>
-        </tr>
-        <tr>
-        <td><i>Land: </i></td>
-        <td></td>
-        <td> ".$ipdata['country']." (".$ipdata['countryCode'].")</td>
-        </tr>
-        <tr>
-        <td><i>Kontinent: </i></td>
-        <td></td>
-        <td> ".$ipdata['continent']." (".$ipdata['continentCode'].")</td>
-        </tr>
-        <tr>
-        <td><i>Währung</i></td>
-        <td></td>
-        <td> ".$ipdata['currency']."</td>
-        </tr>
-        <tr>
-        <td><i>Zeitzone: </i></td>
-        <td></td>
-        <td> ".$ipdata['timezone']."</td>
-        </tr>
-        <tr>
-        <td><i>ISP</i></td>
-        <td></td>
-        <td> ".$ipdata['isp']."</td>
-        </tr>
-        <tr>
-        <td><i>Organisation</i></td>
-        <td></td>
-        <td> ".$ipdata['org']."</td>
-        </tr>
-        <tr>
-        <td><i>AS Nummer</i></td>
-        <td></td>
-        <td> ".$ipdata['as']."</td>
-        </tr>
-        <tr>
-        <td><i>VPN/Proxy </i></td>
-        <td></td>
-        <td> ".$vpn."</td>
-        </tr>
-        <tr>
-        <td><i>Mobile: </i></td>
-        <td></td>
-        <td> ".$mobile."</td>
-        </tr>
-        <tr>
-        <td><i>Datacenter: </i></td>
-        <td></td>
-        <td> ".$datacenter."</td>
-        </tr>
-        </table>
-        </div>
-        </div>
+             <td><i>PING: </i></td>
+             <td></td>
+             <td><span id='ping' class='testping' onclick='testping(&#34;".$query."&#34;)'>Test Ping</span></td>
+             </tr>
+             <tr>
+             <td><i>LAT/LON: </i></td>
+             <td></td>
+             <td> ".$ipdata['lat']."/".$ipdata['lon']."</td>
+             </tr>
+             <tr>
+             <td><i>City: </i></td>
+             <td></td>
+             <td> ".$ipdata['city']."</td>
+             </tr>
+             <tr>
+             <td><i>Postcode: </i></td>
+             <td></td>
+             <td> ".$ipdata['zip']."</td>
+         </tr>
+         <tr>
+         <td><i>Region: </i></td>
+         <td></td>
+         <td> ".$ipdata['regionName']." (".$ipdata['region'].")</td>
+         </tr>
+         <tr>
+         <td><i>Country: </i></td>
+         <td></td>
+         <td> ".$ipdata['country']." (".$ipdata['countryCode'].")</td>
+         </tr>
+         <tr>
+         <td><i>Continent: </i></td>
+         <td></td>
+         <td> ".$ipdata['continent']." (".$ipdata['continentCode'].")</td>
+         </tr>
+         <tr>
+         <td><i>Currency</i></td>
+         <td></td>
+         <td> ".$ipdata['currency']."</td>
+         </tr>
+         <tr>
+         <td><i>Time zone: </i></td>
+         <td></td>
+         <td> ".$ipdata['timezone']."</td>
+         </tr>
+         <tr>
+         <td><i>ISP</i></td>
+         <td></td>
+         <td> ".$ipdata['isp']."</td>
+         </tr>
+         <tr>
+         <td><i>Organization</i></td>
+         <td></td>
+         <td> ".$ipdata['org']."</td>
+         </tr>
+         <tr>
+         <td><i>AS number</i></td>
+         <td></td>
+         <td> ".$ipdata['as']."</td>
+         </tr>
+         <tr>
+         <td><i>VPN/Proxy</i></td>
+         <td></td>
+         <td> ".$vpn."</td>
+         </tr>
+         <tr>
+         <td><i>Mobile: </i></td>
+         <td></td>
+         <td> ".$mobile."</td>
+         </tr>
+         <tr>
+         <td><i>Data center: </i></td>
+         <td></td>
+         <td> ".$datacenter."</td>
+         </tr>
+         </table>
+         </div>
+         </div>
 
         ";
     } else {
         echo '<div class="center"><h3>Deine Anfrage ist Fehlgeschlagen!</h3></div>';
     }
         
-    } else {
-        
-        $ipdata = [];
-        $domain = $_SERVER['REMOTE_ADDR'];
-        
-        $ipdatajson = file_get_contents('http://ip-api.com/json/'.$domain.'?fields=28569599');
-        $ipdata = json_decode($ipdatajson, true);
+   
 
-        if($ipdata['hosting'] == 'true') {
-            $datacenter = '✔';
-        } else {
-            $datacenter = '✘';
-        }
-        if($ipdata['mobile'] == 'true') {
-            $mobile = '✔';
-        } else {
-            $mobile = '✘';
-        }
-        if($ipdata['proxy'] == 'true') {
-            $vpn = '✔';
-        } else {
-            $vpn = '✘';
-        }
-
-        echo "
-        <div class='center'>
-        <h3>Deine IP Adresse (".$ipdata['query'].") gehört zu dieser Region:</h3>
-        <div id='map'></div>
-        </div>
-        <script>
-        var map = L.map('map').setView([".$ipdata['lat'].", ".$ipdata['lon']."], 13);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap'
-        }).addTo(map);
-        L.marker([".$ipdata['lat'].", ".$ipdata['lon']."]).addTo(map)
-        .bindPopup('<table><tr><td></td><td><b>".$ipdata['query']."</b></td></tr><tr><td><i>Stadt:</i></td><td>".$ipdata['city']." (".$ipdata['zip'].")</td></tr><tr><td><i>Region:</i></td><td>".$ipdata['regionName']." (".$ipdata['region'].")</td></tr><tr><td><i>Land: </i></td><td>".$ipdata['country']." (".$ipdata['countryCode'].")</td></tr><tr><td><i>Zeitzone: </i></td><td>".$ipdata['timezone']."</td></tr><tr><td><i>ISP:</i></td><td>".$ipdata['isp']."</td></tr><tr><td><i>Datacenter: </i></td><td>".$datacenter."</td></tr><tr><td><i>Mobil: </i></td><td>".$mobile."</td></tr></table>')
-        .openPopup();
-        </script>
-
-        <div class='center'>
-        <h2>Genaue Informationen</h2>
-        <div class='exactinfo'>
-        <table>
-            <tr>
-                <td><i>Suche: </i></td>
-                <td></td>
-                <td>Deine IP (".$ipdata['query'].")</td>
-            </tr>
-            <tr>
-            <td><i>IP: </i></td>
-            <td></td>
-            <td> ".$ipdata['query']."</td>
-            </tr>
-            <tr>
-            <td><i>LAT/LON: </i></td>
-            <td></td>
-            <td> ".$ipdata['lat']."/".$ipdata['lon']."</td>
-            </tr>
-            <tr>
-            <td><i>Stadt: </i></td>
-            <td></td>
-            <td> ".$ipdata['city']."</td>
-            </tr>
-            <tr>
-            <td><i>Postleitzahl: </i></td>
-            <td></td>
-            <td> ".$ipdata['zip']."</td>
-        </tr>
-        <tr>
-        <td><i>Region: </i></td>
-        <td></td>
-        <td> ".$ipdata['regionName']." (".$ipdata['region'].")</td>
-        </tr>
-        <tr>
-        <td><i>Land: </i></td>
-        <td></td>
-        <td> ".$ipdata['country']." (".$ipdata['countryCode'].")</td>
-        </tr>
-        <tr>
-        <td><i>Kontinent: </i></td>
-        <td></td>
-        <td> ".$ipdata['continent']." (".$ipdata['continentCode'].")</td>
-        </tr>
-        <tr>
-        <td><i>Währung</i></td>
-        <td></td>
-        <td> ".$ipdata['currency']."</td>
-        </tr>
-        <tr>
-        <td><i>Zeitzone: </i></td>
-        <td></td>
-        <td> ".$ipdata['timezone']."</td>
-        </tr>
-        <tr>
-        <td><i>ISP</i></td>
-        <td></td>
-        <td> ".$ipdata['isp']."</td>
-        </tr>
-        <tr>
-        <td><i>Organisation</i></td>
-        <td></td>
-        <td> ".$ipdata['org']."</td>
-        </tr>
-        <tr>
-        <td><i>AS Nummer</i></td>
-        <td></td>
-        <td> ".$ipdata['as']."</td>
-        </tr>
-        <tr>
-        <td><i>VPN/Proxy </i></td>
-        <td></td>
-        <td> ".$vpn."</td>
-        </tr>
-        <tr>
-        <td><i>Mobile: </i></td>
-        <td></td>
-        <td> ".$mobile."</td>
-        </tr>
-        <tr>
-        <td><i>Datacenter: </i></td>
-        <td></td>
-        <td> ".$datacenter."</td>
-        </tr>
-        </table>
-        </div>
-        </div>
-
-        ";
-        
-    }
-
-    if(isset($_GET['query'])) {
-        $whois = 'whois/?query='.$_GET['query'].'';
-    } else {
-        $reversedns = 'reversedns/';
-    }
-echo '
-<div class="center">
-	<footer>
-        < <a href="https://github.com/LarvenStein/IP-Lookup" target="_blank">GitHub</a> / <a href="https://discord.com/api/oauth2/authorize?client_id=992069594900611213&permissions=67584&scope=bot" target="_blank">Discord Bot</a> >
-	</footer>
-    </div>
+    echo '
+    <div class="center">
     ';
-    ?>
+        include 'footer.php';
+?> 
+    </div> 
 </body>
 </html>
